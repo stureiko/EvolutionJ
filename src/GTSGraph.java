be566a2b8758f4cf13f061bf3f3700321d50eead
Igor Stureyko <stureiko@gmail.com>
2018-06-18 19:20:24 +0300

    add class for graph

---
+++ src/GTSGraph.java
@@ -0,0 +1,3 @@
+public class GTSGraph {
+///TODO: add nodes and lines
+}
--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+//    /TODO: add classes for nodes and lines of graph
 }
