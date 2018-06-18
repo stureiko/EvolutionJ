bbc5a98a46d21a3688e98c5563308fbce5327dbd
Igor Stureyko <stureiko@gmail.com>
2018-06-18 21:43:42 +0300

    add class for GTSNode

---
+++ src/GTSNode.java
@@ -0,0 +1,27 @@
+public class GTSNode {
+    private int x, y, id;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
