8d941a80c3e3c62250427a5a2546932dc8d42201
Igor Stureyko <stureiko@gmail.com>
2018-06-19 19:38:09 +0300

    add variables to GTSNode

--- src/GTSGraph.java
+++ src/GTSGraph.java
@@ -1,3 +1,6 @@
 public class GTSGraph {
-///TODO: add nodes and lines
+//          /TODO: add nodes and lines
+    public void loadGraph(){
+//          /TODO add implementation in a separate thread
+    }
 }
--- src/GTSNode.java
+++ src/GTSNode.java
@@ -1,5 +1,6 @@
 public class GTSNode {
     private int x, y, id;
+    private String name;
 
     public int getId() {
         return id;
@@ -23,5 +24,39 @@ public class GTSNode {
 
     public void setY(int y) {
         this.y = y;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public GTSNode(){
+        x = 0;
+        y = 0;
+        name = "";
+        id = 0;
+    }
+
+    public GTSNode(int x, int y, String name, int id){
+        this.x = x;
+        this.y = y;
+        this.name = name;
+        this.id = id;
+    }
+
+    public GTSNode(GTSNode node){
+        this.x = node.x;
+        this.y = node.y;
+        this.name = "";
+        this.id = 0;
+    }
+
+    public void printCoordinates(){
+        System.out.println(x);
+        System.out.println(y);
     }
 }
