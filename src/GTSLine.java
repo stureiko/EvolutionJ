71f22fed42b3995c020108c0d82d6c86b412491d
Igor Stureiko <stureiko@mail.ru>
2018-06-20 15:02:23 +0300

    add class GTSLine

---
+++ src/GTSLine.java
@@ -0,0 +1,22 @@
+public class GTSLine {
+    private GTSNode startNode;
+    private GTSNode finishNode;
+
+    public GTSNode getStartNode() {
+        return startNode;
+    }
+
+    public void setStartNode(GTSNode startNode) {
+        this.startNode = startNode;
+    }
+
+    public GTSNode getFinishNode() {
+        return finishNode;
+    }
+
+    public void setFinishNode(GTSNode finishNode) {
+        this.finishNode = finishNode;
+    }
+
+
+}
